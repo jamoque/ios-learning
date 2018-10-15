fc4b03e47a615bece814dca679e0953ddedf7faa
jeremy.hintz <jeremy.hintz@uber.com>
2018-10-14 22:07:20 -0700

    swift book ch 2 playground

---
+++ swift-exercises/Ch2Variables.playground/Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+/* 2.1 - 2.5 */
+//var numberOfStoplights = "Four"
+//var numberOfStoplights: Int = 700
+//let numberOfStoplights: Int = 4
+//numberOfStoplights += 2
+
+/* 2.6 - 2.9 */
+let numberOfStoplights: Int = 4
+var population: Int
+population = 5422
+let townName: String = "Knowhere"
+let townDescription =
+"\(townName) has a population of \(population) and \(numberOfStoplights) stoplights."
+print(townDescription)
+
+/* Bronze challenge */
+var unemploymentRate : Double = 0.05
